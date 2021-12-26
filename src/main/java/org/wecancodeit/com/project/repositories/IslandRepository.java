4114601d2800f54f005f86c4ccd8bd5c3f7b9ecb
JediJake66 <jacobstout59@gmail.com>
2021-12-26 11:56:04 -0500

    completed island controller and repo

---
+++ src/main/java/org/wecancodeit/com/project/repositories/IslandRepository.java
@@ -0,0 +1,8 @@
+package org.wecancodeit.com.project.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.wecancodeit.com.project.models.Island;
+
+public interface IslandRepository extends CrudRepository<Island, Long> {
+    Island findIslandByName(String name);
+}
